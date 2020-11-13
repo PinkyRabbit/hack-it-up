b34b56559daeafb5d3667a81fbd306d8217891c1
Mikita Melnikau <usesa@yandex.com>
2020-11-13 12:21:58 +0300

    production middleware added

--- app/index.js
+++ app/index.js
@@ -3,15 +3,23 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const createError = require('http-errors');
+const helmet = require('helmet');
 
 const initSession = require('./middlewares/session');
 const initFlash = require('./middlewares/flash');
+const useCompression = require('./middlewares/compression');
 
 const errorHandler = require('./pages/error');
 
 const pages = require('../pages.json');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+  app.use(helmet());
+  useCompression(app);
+}
 
 const rootDirectory = path.resolve(__dirname, '..');
 app.use(express.static(path.join(rootDirectory, 'public')));
